f05ebc15580f16afae7beda1a3c637a11932f8ed
Anass <shaikanass304@gmail.com>
2023-05-11 22:01:22 +0530

    terraform provisioner

---
+++ 10-provisioner/mai.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags = {
+    Name = "demo"
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      host     = self.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "echo Hello"
+    ]
+  }
+
+}
+
+
