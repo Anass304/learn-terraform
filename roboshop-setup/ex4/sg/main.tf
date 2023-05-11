9d43c9c942adcab267185d018960d6044c3a7508
Anass <shaikanass304@gmail.com>
2023-05-11 22:08:00 +0530

    security groups

---
+++ roboshop-setup/ex4/sg/main.tf
@@ -0,0 +1,26 @@
+module "ec2" {
+  for_each      = var.instances
+  source        = "./ec2"
+  component     = each.value["name"]
+  instance_type = each.value["type"]
+  sg_id         = module.sg.sg_id
+}
+
+module "sg" {
+  source = "./sg"
+}
+
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
+
